8712c98fb79782ebfa67cdf74cb378a900b9eded
ofdun <elik394@mail.ru>
2023-06-22 23:12:15 +0300

    uploading videos to a server

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
@@ -10,5 +10,14 @@ def index():
 def list_():
     return render_template('list.html')
 
+@app.route('/videos/glitch.mp4')
+def video():
+    return send_file('videos/glitch.mp4', mimetype='video/mp4')
+
+@app.route('/videos/glitch2.mp4')
+def video2():
+    return send_file('videos/asdasd.mp4', mimetype='video/mp4')
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
+    
